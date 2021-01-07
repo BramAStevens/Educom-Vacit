5933ee764e935308ac838b9fe1089d5f0a75c881
BramAStevens <60523687+BramAStevens@users.noreply.github.com>
2021-01-07 16:27:34 +0100

    post requests work! register & updateprofile complete

--- Vacit/src/Controller/UserController.php
+++ Vacit/src/Controller/UserController.php
@@ -14,21 +14,18 @@ use App\Entity\User;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/saveUser", name="saveUser")
+     * @Route("/registerUser", name="registerUser")
      */
-    public function saveUser(UserService $userService)
+    public function registerUser(UserService $userService, Request $request)
     {
-        $params = array(
-            
-            "username" => "username64",
-            "password" => "blabla",
-
-        );
-
-       // $rep = $this->getDoctrine()->getRepository(User::class); //do this with post
-        $user = $userService->saveUser($params);
-        dump($user);
-        die();
+     
+        $params["username"] = $request->request->get("username");
+        $params["password"] = $request->request->get("password");    
+    
+        $data = $userService->saveUser($params);
+      
+        return $this->render('user/register.html.twig');
+       
     }
 
     /**
@@ -36,36 +33,47 @@ class UserController extends AbstractController
      */
     public function showUsers(UserService $userService) {
         
-      //  $rep = $this->getDoctrine()->getRepository(User::class);
         $user = $userService->getAllUsers();
-
         dump($user);
         die();
     }
     /**
      * @Route("/updateUserProfile/{id}", name="updateUserProfile")
      */
-    public function updateUserProfile(UserService $userService, $id) {
-        $params = array(
+    public function updateUserProfile(Request $request, UserService $userService, $id) {
+        // $params = array(
 
-            "id" => $id,
-            "user_picture" => "NO",
-            "user_surname" => "surname1",
-            "user_lastname" => "lastname1",
-            "user_email" => "email1",
-            "user_dob" => "10-05-1990",
-            "user_phone_number" => "12324",
-            "user_address" => "address1",
-            "user_postcode" => "postcode1",
-            "user_city" => "city1",
-            "user_motivation" => "motivation1",
-            "user_cv" => "cv1",
+        //     "id" => $id,
+        //     "user_picture" => "NO",
+        //     "user_surname" => "surname1",
+        //     "user_lastname" => "lastname1",
+        //     "user_email" => "email1",
+        //     "user_dob" => "10-05-1990",
+        //     "user_phone_number" => "12324",
+        //     "user_address" => "address1",
+        //     "user_postcode" => "postcode1",
+        //     "user_city" => "city1",
+        //     "user_motivation" => "motivation1",
+        //     "user_cv" => "cv1",
 
-        );
+        // );
+        
+        $params["id"] = $id;
+        $params["user_picture"] = $request->request->get("user_picture");
+        $params["user_surname"] = $request->request->get("user_surname");    
+        $params["user_lastname"] = $request->request->get("user_lastname");
+        $params["user_email"] = $request->request->get("user_email");
+        $params["user_dob"] = $request->request->get("user_dob");
+        $params["user_phone_number"] = $request->request->get("user_phone_number");
+        $params["user_address"] = $request->request->get("user_address");
+        $params["user_postcode"] = $request->request->get("user_postcode");
+        $params["user_city"] = $request->request->get("user_city");
+        $params["user_motivation"] = $request->request->get("user_surname");    
+        $params["user_cv"] = $request->request->get("user_cv");
+
         $user = $userService->findUserById($id);
         $userService->updateUserProfile($params);
-        dump($user);
-        die();
+        return $this->render('user/profile.html.twig');
     }
 
     /**
